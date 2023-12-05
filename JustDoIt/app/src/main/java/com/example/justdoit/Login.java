28c3711bdc5d9bbc2cde3ce8bf06b57e0240c373
Park Junseok <20191271@kookmin.ac.kr>
2023-12-05 22:04:39 +0900

    1206

--- JustDoIt/app/src/main/java/com/example/justdoit/Login.java
+++ JustDoIt/app/src/main/java/com/example/justdoit/Login.java
@@ -11,11 +11,11 @@ package com.example.justdoit;
         import androidx.appcompat.app.AppCompatActivity;
 
         import com.android.volley.Request;
-        import com.android.volley.toolbox.StringRequest;
         import com.android.volley.toolbox.Volley;
+        import com.android.volley.toolbox.JsonObjectRequest;
+        import org.json.JSONException;
+        import org.json.JSONObject;
 
-        import java.util.HashMap;
-        import java.util.Map;
 
 public class Login extends AppCompatActivity {
     private static final String LOGIN_URL = "http://10.0.2.2:3123/login";
@@ -52,29 +52,33 @@ public class Login extends AppCompatActivity {
     private void login() {
         String userId = editID.getText().toString();
         String password = ediPassword.getText().toString();
+        String url = LOGIN_URL;
 
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, LOGIN_URL, response -> {
+        JSONObject loginParams = new JSONObject();
+        try {
+            loginParams.put("user_name", userId);
+            loginParams.put("user_pw", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, loginParams,
+                response -> {
                     // 서버 응답 처리
-                    if (response.contains("True")) {
+                    if (response.toString().contains("True")) {
                         Intent intent = new Intent(Login.this, MainActivity.class);
                         startActivity(intent);
                         finish();
                     } else {
-                        Toast.makeText(Login.this, "로그인 실패: " + response, Toast.LENGTH_LONG).show();
+                        Toast.makeText(Login.this, "로그인 실패", Toast.LENGTH_LONG).show();
                     }
                 },
                 error -> {
-                    // 에러 처리 개선
+                    // 에러 처리
                     Toast.makeText(Login.this, "네트워크 오류가 발생했습니다. 다시 시도해주세요.", Toast.LENGTH_LONG).show();
-                }) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<>();
-                params.put("user_id", userId);
-                params.put("user_pw", password);
-                return params;
-            }
-        };
-        Volley.newRequestQueue(this).add(stringRequest);
+                }
+        );
+
+        Volley.newRequestQueue(this).add(jsonObjectRequest);
     }
 }
